c6a1f3d055239fcc15302cbce87925bb05fd4f1e
Tanvir Ahamed <155292317+TajTanvir1@users.noreply.github.com>
2024-07-04 18:07:10 +0600

    Update Footer

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,9 +1,10 @@
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="items-end align-bottom">
-      <footer className="px-4 divide-y dark:bg-orange-50 dark:text-orange-950">
+      <footer className="px-4 divide-y dark:bg-sky-50 dark:text-green-950">
         <div className="container flex flex-col justify-between py-6 mx-auto space-y-4 lg:flex-row lg:space-y-0">
           <div className="lg:w-1/3">
             <a
@@ -11,79 +12,31 @@ const Footer = () => {
               href="#"
               className="flex justify-center space-x-3 lg:justify-start"
             >
-              <span className="self-center  font-kanit text-3xl font-bold dark-color underline">
+              <span className="self-center  font-kanit text-xl font-bold dark-color underline">
                 <img
-                  src="https://i.ibb.co/mcStBK9/EA-Logo.png"
+                  src="https://i.ibb.co/tbbkjfp/Tanvir-Passport-Pic-C.jpg"
                   alt=""
-                  className="w-12 mx-auto"
+                  className="w-12 mx-auto rounded-lg"
                 />
-                Explore Alternate
+                Tanvir Ahamed Portfolio
               </span>
             </a>
           </div>
-          <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
-            <div className="space-y-3">
-              <h3 className="tracking-wide uppercase dark:text-gray-900 font-bold">
-                Product
-              </h3>
-              <ul className="space-y-1">
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Features
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Integrations
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Pricing
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    FAQ
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="space-y-3">
-              <h3 className="tracking-wide uppercase dark:text-gray-900 font-bold">
-                Company
-              </h3>
-              <ul className="space-y-1">
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Privacy
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Terms of Service
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div className="flex flex-row justify-around text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+            
             <div className="space-y-3">
               <h3 className="uppercase dark:text-gray-900 font-bold">
-                Developers
+                Contact Info
               </h3>
               <ul className="space-y-1">
                 <li>
                   <a rel="noopener noreferrer" href="#">
-                    Public API
+                    +880 1408 778787
                   </a>
                 </li>
                 <li>
                   <a rel="noopener noreferrer" href="#">
-                    Documentation
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Guides
+                    tanvirah1223@gmail.com
                   </a>
                 </li>
               </ul>
@@ -95,34 +48,31 @@ const Footer = () => {
               <div className="flex justify-start space-x-3 text-xl">
                 <a
                   rel="noopener noreferrer"
-                  href="#"
+                  href="https://www.linkedin.com/in/tanvirahamedjubayer/"
+                  title="Linkedin"
+                  className="flex items-center p-1"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  rel="noopener noreferrer"
+                  href="https://github.com/TajTanvir1"
+                  title="Github"
+                  className="flex items-center p-1"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/taj.tanvir11"
                   title="Facebook"
                   className="flex items-center p-1"
                 >
                   <FaFacebook />
                 </a>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  title="Twitter"
-                  className="flex items-center p-1"
-                >
-                  <FaTwitter />
-                </a>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  title="Instagram"
-                  className="flex items-center p-1"
-                >
-                  <FaInstagram />
-                </a>
               </div>
             </div>
           </div>
-        </div>
-        <div className="py-3 text-sm text-center dark:text-gray-600">
-          © All rights reserved to Explore Alternate.
         </div>
       </footer>
     </div>
